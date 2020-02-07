a62cb9637d99b0a759d4ad5645d885ba7ee251df
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-07 09:04:20 -0500

    instrumented berkeley, but it is crashing

--- Phosphor/scripts/instrument/control/instrument_project.sh
+++ Phosphor/scripts/instrument/control/instrument_project.sh
@@ -38,10 +38,9 @@ function inst() {
   local jar=$2
   local dir=$3
 
-  java -Xmx10g -jar $phosphor_jar -withCCSinks -controlTrack -withoutBranchNotTaken -forceUnboxAcmpEq -withEnumsByValue ./target/$jar $dir
+  java -Xmx10g -jar $phosphor_jar -controlTrack -withoutBranchNotTaken -forceUnboxAcmpEq -withEnumsByValue ./target/$jar $dir
+#  java -Xmx10g -jar $phosphor_jar -withCCSinks -controlTrack -withoutBranchNotTaken -forceUnboxAcmpEq -withEnumsByValue ./target/$jar $dir
   #    java -Xmx10g -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -jar $phosphor_jar -withCCSinks -controlTrack -withoutBranchNotTaken -forceUnboxAcmpEq -withEnumsByValue ./target/$jar $dir
-
-  #  java -Xmx10g -jar $phosphor_jar -priorClassVisitor edu.cmu.cs.mvelezce.cc.instrumenter.CCStaticFieldAdderClassVisitor -controlFlowManager edu.cmu.cs.mvelezce.cc.control.CCStandardControlFlowManager -withCCSinks -withoutBranchNotTaken -forceUnboxAcmpEq -withEnumsByValue ./target/$jar $dir
 }
 
 (
