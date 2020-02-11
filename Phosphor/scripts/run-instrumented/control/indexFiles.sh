ce0626178ff8443fc9896edcd80deb626dacf23c
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-11 08:34:29 -0500

    instrumented lucene

--- Phosphor/scripts/run-instrumented/control/indexFiles.sh
+++ Phosphor/scripts/run-instrumented/control/indexFiles.sh
@@ -24,29 +24,28 @@ PROGRAM_JAR=$BASE/../../../examples/control/lucene-7.4.0.jar
 MAIN_CLASS=${ARG0}
 PROGRAM_NAME=${MAIN_CLASS##*.}
 
-function run {
-    local program_jar=$1
-    local main_class=$2
-    local arg0=$3
-    local arg1=$4
-    local arg2=$5
-    local arg3=$6
-    local arg4=$7
-    local arg5=$8
-    local arg6=$9
-    local arg7=${10}
-    local arg8=${11}
-    local arg9=${12}
-    local arg10=${13}
-    local arg11=${14}
-    local arg12=${15}
-    local arg13=${16}
-    local arg14=${17}
-    local arg15=${18}
-    local arg16=${19}
+function run() {
+  local program_jar=$1
+  local main_class=$2
+  local arg0=$3
+  local arg1=$4
+  local arg2=$5
+  local arg3=$6
+  local arg4=$7
+  local arg5=$8
+  local arg6=$9
+  local arg7=${10}
+  local arg8=${11}
+  local arg9=${12}
+  local arg10=${13}
+  local arg11=${14}
+  local arg12=${15}
+  local arg13=${16}
+  local arg14=${17}
+  local arg15=${18}
+  local arg16=${19}
 
-    ../../../target/jre-inst-implicit-optimized/bin/java -Xmx28g -Xms28g -XX:+UseConcMarkSweepGC -Xbootclasspath/a:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -javaagent:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -cp $program_jar $main_class $arg0 $arg1 $arg2 $arg3 $arg4 $arg5 $arg6 $arg7 $arg8 $arg9 $arg10 $arg11 $arg12 $arg13 $arg14 $arg15 $arg16
-#    ../../../target/jre-inst-implicit-optimized/bin/java -Xmx28g -Xms28g -XX:+UseConcMarkSweepGC -Xbootclasspath/a:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -javaagent:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -cp $program_jar $main_class $arg0 $arg1 $arg2 $arg3 $arg4 $arg5 $arg6 $arg7 $arg8 $arg9 $arg10 $arg11 $arg12 $arg13 $arg14 $arg15 $arg16
+  ../../../../integration-tests/target/jvm-inst-optimized-control/bin/java -Xmx28g -Xms28g -Xbootclasspath/a:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -javaagent:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -cp $program_jar $main_class $arg0 $arg1 $arg2 $arg3 $arg4 $arg5 $arg6 $arg7 $arg8 $arg9 $arg10 $arg11 $arg12 $arg13 $arg14 $arg15 $arg16
 }
 
 rm -rf $BASE/../../../examples/control/$PROGRAM_NAME
