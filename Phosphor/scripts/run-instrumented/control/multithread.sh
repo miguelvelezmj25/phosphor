4ee8a89a7e13bbc9dccc5fca05e5c643877cbd64
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-18 11:22:16 -0500

    analyzed multithreaded program

---
+++ Phosphor/scripts/run-instrumented/control/multithread.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+ARG0=${1}
+ARG1=${2}
+ARG2=${3}
+ARG3=${4}
+ARG4=${5}
+ARG5=${6}
+
+BASE=$(pwd)
+PROGRAM_JAR=$BASE/../../../examples/control/multithread-taint-0.1.0-SNAPSHOT.jar
+MAIN_CLASS=${ARG0}
+PROGRAM_NAME=${MAIN_CLASS##*.}
+
+function run() {
+  local program_jar=$1
+  local main_class=$2
+  local arg0=$3
+  local arg1=$4
+  local arg2=$5
+  local arg3=$6
+  local arg4=$7
+
+  ../../../../integration-tests/target/jvm-inst-optimized-control/bin/java -Xmx28g -Xms28g -Xbootclasspath/a:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -javaagent:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -cp $program_jar $main_class $arg0 $arg1 $arg2 $arg3 $arg4
+  #  ../../../../integration-tests/target/jvm-inst-optimized-control/bin/java -Xmx28g -Xms28g -Xbootclasspath/a:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -javaagent:../../../target/Phosphor-0.0.5-SNAPSHOT.jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -cp $program_jar $main_class $arg0 $arg1 $arg2 $arg3 $arg4
+}
+
+rm -rf $BASE/../../../examples/control/$PROGRAM_NAME
+mkdir $BASE/../../../examples/control/$PROGRAM_NAME
+time run $PROGRAM_JAR $MAIN_CLASS $ARG1 $ARG2 $ARG3 $ARG4 $ARG5
